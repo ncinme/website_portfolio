4366979c414003c4dea0ffa695a9d6ac88b1c12d
ncinme <ncinme2020@gmail.com>
2022-01-01 21:43:52 +0530

    Added logging and exception handling

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import smtplib
-
+import logging
 import os
-from dotenv import load_dotenv      # pip install python-dotenv
+from dotenv import load_dotenv  # pip install python-dotenv
+
+LOG_FORMAT = "%(levelname)s %(asctime)s -- %(message)s"
+logging.basicConfig(filename='portfolio_website.log', level=logging.INFO, format=LOG_FORMAT, filemode='a')
 
 load_dotenv()
 
-
 app = Flask(__name__)
-
-app.config['MY_EMAIL'] = os.environ.get("MY_EMAIL")       # read from .env file
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")  # read from .env file
+app.config['MY_EMAIL'] = os.environ.get("MY_EMAIL")
 app.config['MY_PASSWORD'] = os.environ.get("MY_PASSWORD")
 
 
@@ -20,30 +22,40 @@ def home():
 
 @app.route('/', methods=['POST'])
 def receive_data():
-    data = request.form
-    send_email(data)
-    return render_template('index.html', inputdata=data)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        msg = request.form['message']
+        if name != '' or email != '' or msg != '':
+            send_email(name, email, msg)
+
+    return render_template('index.html')
 
 
-def send_email(data):
-    name = data['name']
-    email = data['email']
-    msg = data['message']
-
+def send_email(name, email, msg):
     message = f'Thank you for contacting me. I have received following information from you:' \
               f'\n\nName: {name}, \nEmail: {email}\nMessage: {msg}' \
               f'\n\nI will soon respond to your message.\n\nNC InMe'
 
-    with smtplib.SMTP('smtp.gmail.com') as connection:
-        connection.starttls()
-        connection.login(user=app.config['MY_EMAIL'], password=app.config['MY_PASSWORD'])
-        connection.sendmail(
-            from_addr=app.config['MY_EMAIL'],
-            to_addrs=email,
-            msg=f'Subject: Acknowledgement from NC InMe\n\n{message}'
-        )
+    try:
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=app.config['MY_EMAIL'], password=app.config['MY_PASSWORD'])
+            connection.sendmail(
+                from_addr=app.config['MY_EMAIL'],
+                to_addrs=email,
+                msg=f'Subject: Acknowledgement from NC InMe\n\n{message}'
+            )
+    except KeyError as err:
+        logging.exception(err)
+        flash(f"Error sending email. Please try after some time.")
+
+    except Exception as err:
+        logging.exception(err)
+        flash(f"Error sending message. Please try again.")
+    else:
+        flash(f"Thank you for contacting me {name}. I will revert soon.")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
